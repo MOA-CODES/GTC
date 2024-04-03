128b017c2139c00badd4c7a04fde7fc9d14d5c56
MOA -CODES <moyo.ajla@gmail.com>
2024-04-03 13:19:10 +0100

    STILL "error at Query.run"

--- controllers/Auth_C.js
+++ controllers/Auth_C.js
@@ -4,11 +4,11 @@ const {StatusCodes} = require('http-status-codes')
 
 const User = require('../models/User_M')
 
-const register = async (req, res) => {
+const register = async (req, res)=> {
 
     const user = await User.create(req.body)
 
-    res.status(StatusCodes.OK).send
+    res.status(StatusCodes.CREATED).json({msg:"user created successfully",user})
 }
 
 const login = async (req, res) => {
